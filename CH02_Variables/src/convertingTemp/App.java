31c86b99739754344595698f0d9e5f0f13e50d11
LJO <liv01080208@gmail.com>
2021-10-13 12:31:53 +0900

    10월13일오전2

---
+++ CH02_Variables/src/convertingTemp/App.java
@@ -0,0 +1,14 @@
+package convertingTemp;
+
+public class App {
+
+	public static void main(String[] args) {
+		// ¼·¾¾ Celsius => È­¾¾ Fahrenheit
+		double c = 25; //¼·¾¾
+		double f = (c*9/5) + 32; //È­¾¾
+		
+		System.out.println("¼·¾¾ "+c+"´Â È­¾¾ "+f+"ÀÌ´Ù.");
+
+	}
+
+}
