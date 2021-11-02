c89889bb9c7f2621cd3751ab27590e724599a042
ok-olly <liv01080208@gmail.com>
2021-11-02 15:26:59 +0900

    1102 오후

---
+++ CH23_CollectionsAPI/src/hashmap/Iterating_Map.java
@@ -0,0 +1,35 @@
+package hashmap;
+
+import java.util.HashMap;
+
+public class Iterating_Map {
+	public static void main(String[] args) {
+		//반복문으로 맵의 아이템들을 전부 읽는 방법
+		HashMap<String, String> months = new HashMap<>();
+		months.put("JAN", "1월");
+		months.put("FAB", "2월");
+		months.put("MAR", "3월");
+		months.put("APR", "4월");
+		months.put("MAY", "5월");
+		months.put("JUN", "6월");
+		
+		// 1.람다식
+		months.forEach((code, name) -> System.out.println(code+": "+name));
+		System.out.println();
+		
+		// 2.entrySet() : key와 value값을 가져온다.
+		for(var entry : months.entrySet()) {
+			String k = entry.getKey(); //키값
+			String v = entry.getValue(); //밸류값
+			System.out.println(k+": "+v);
+		}
+		System.out.println();
+		
+		// 3.keySet() : key값을 가져옴
+		for(String k : months.keySet()) {
+			String v = months.get(k);			
+			System.out.println(k+": "+v);
+		}
+	}
+
+}
